57130627457e90f671a44add5ad676b1d28d94fc
rppicomidi <rppicomidi@gmail.com>
2023-07-26 07:50:55 -0700

    re-enable MIDI clock

--- btstack_config.h
+++ btstack_config.h
@@ -17,7 +17,7 @@
 // For the BLE-MIDI server
 #define BLE_MIDI_SERVER_MAX_CONNECTIONS 1
 // comment out to enable the MIDI clock real-time message from server to client
-#define BLE_MIDI_SERVER_FILTER_MIDI_CLOCK_TO_BLE
+// #define BLE_MIDI_SERVER_FILTER_MIDI_CLOCK_TO_BLE
 // comment out to enable the Active Sensing real-time message from server to client
 #define BLE_MIDI_SERVER_FILTER_ACTIVE_SENSING_TO_BLE
 
