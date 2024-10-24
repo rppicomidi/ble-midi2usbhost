70ac716129628a43bd6db46e104019ae0738c1ec
rppicomidi <rppicomidi@gmail.com>
2024-10-23 19:15:10 -0700

    integrate updated server library

--- ble-midi2usbhost.c
+++ ble-midi2usbhost.c
@@ -71,7 +71,7 @@
  */
 #include <inttypes.h>
 #include <stdio.h>
-#include "midi_service_stream_handler.h"
+#include "ble_midi_server.h"
 #include "btstack.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
@@ -93,165 +93,12 @@ const uint8_t adv_data_len = sizeof(adv_data);
 
 const uint8_t scan_resp_data[] = {
     // Name
-    0x0E, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'B', 'L', 'E', '-', 'M', 'I', 'D', 'I', '2', 'U', 'S', 'B', 'H',
+    0x11, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'B', 'L', 'E', '-', 'M', 'I', 'D', 'I', '2', 'U', 'S', 'B', 'H','U','B'
 };
 const uint8_t scan_resp_data_len = sizeof(scan_resp_data);
 
-static hci_con_handle_t con_handle = HCI_CON_HANDLE_INVALID;
-
 static uint8_t midi_dev_addr = 0;
 
-static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
-    UNUSED(size);
-    UNUSED(channel);
-    bd_addr_t local_addr;
-    uint8_t event_type;
-    bd_addr_t addr;
-    bd_addr_type_t addr_type;
-    uint8_t status;
-    switch(packet_type) {
-        case HCI_EVENT_PACKET:
-            event_type = hci_event_packet_get_type(packet);
-            switch(event_type){
-                case BTSTACK_EVENT_STATE:
-                    if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) {
-                        return;
-                    }
-                    gap_local_bd_addr(local_addr);
-                    printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
-
-                    // setup advertisements
-                    uint16_t adv_int_min = 800;
-                    uint16_t adv_int_max = 800;
-                    uint8_t adv_type = 0;
-                    bd_addr_t null_addr;
-                    memset(null_addr, 0, 6);
-                    gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0, null_addr, 0x07, 0x00);
-                    assert(adv_data_len <= 31); // ble limitation
-                    gap_advertisements_set_data(adv_data_len, (uint8_t*) adv_data);
-                    assert(scan_resp_data_len <= 31); // ble limitation
-                    gap_scan_response_set_data(scan_resp_data_len, (uint8_t*) scan_resp_data);
-                    gap_advertisements_enable(1);
-
-                    break;
-                case HCI_EVENT_DISCONNECTION_COMPLETE:
-                    printf("ble-midi2usbhost: HCI_EVENT_DISCONNECTION_COMPLETE event\r\n");
-                    break;
-                case HCI_EVENT_GATTSERVICE_META:
-                    switch(hci_event_gattservice_meta_get_subevent_code(packet)) {
-                        case GATTSERVICE_SUBEVENT_SPP_SERVICE_CONNECTED:
-                            con_handle = gattservice_subevent_spp_service_connected_get_con_handle(packet);
-                            break;
-                        case GATTSERVICE_SUBEVENT_SPP_SERVICE_DISCONNECTED:
-                            printf("ble-midi2usbhost: GATTSERVICE_SUBEVENT_SPP_SERVICE_DISCONNECTED event\r\n");
-                            con_handle = HCI_CON_HANDLE_INVALID;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case SM_EVENT_JUST_WORKS_REQUEST:
-                    printf("ble-midi2usbhost: Just Works requested\n");
-                    sm_just_works_confirm(sm_event_just_works_request_get_handle(packet));
-                    break;
-                case SM_EVENT_NUMERIC_COMPARISON_REQUEST:
-                    printf("ble-midi2usbhost: Confirming numeric comparison: %"PRIu32"\n", sm_event_numeric_comparison_request_get_passkey(packet));
-                    sm_numeric_comparison_confirm(sm_event_passkey_display_number_get_handle(packet));
-                    break;
-                case SM_EVENT_PASSKEY_DISPLAY_NUMBER:
-                    printf("ble-midi2usbhost: Display Passkey: %"PRIu32"\n", sm_event_passkey_display_number_get_passkey(packet));
-                    break;
-                case SM_EVENT_IDENTITY_CREATED:
-                    sm_event_identity_created_get_identity_address(packet, addr);
-                    printf("ble-midi2usbhost: Identity created: type %u address %s\n", sm_event_identity_created_get_identity_addr_type(packet), bd_addr_to_str(addr));
-                    break;
-                case SM_EVENT_IDENTITY_RESOLVING_SUCCEEDED:
-                    sm_event_identity_resolving_succeeded_get_identity_address(packet, addr);
-                    printf("ble-midi2usbhost: Identity resolved: type %u address %s\n", sm_event_identity_resolving_succeeded_get_identity_addr_type(packet), bd_addr_to_str(addr));
-                    break;
-                case SM_EVENT_IDENTITY_RESOLVING_FAILED:
-                    sm_event_identity_created_get_address(packet, addr);
-                    printf("ble-midi2usbhost: Identity resolving failed\n");
-                    break;
-                case SM_EVENT_PAIRING_STARTED:
-                    printf("Pairing started\n");
-                    break;
-                case SM_EVENT_PAIRING_COMPLETE:
-                    switch (sm_event_pairing_complete_get_status(packet)){
-                        case ERROR_CODE_SUCCESS:
-                            printf("ble-midi2usbhost: Pairing complete, success\n");
-                            break;
-                        case ERROR_CODE_CONNECTION_TIMEOUT:
-                            printf("ble-midi2usbhost: Pairing failed, timeout\n");
-                            break;
-                        case ERROR_CODE_REMOTE_USER_TERMINATED_CONNECTION:
-                            printf("ble-midi2usbhost: Pairing failed, disconnected\n");
-                            break;
-                        case ERROR_CODE_AUTHENTICATION_FAILURE:
-                            printf("ble-midi2usbhost: Pairing failed, authentication failure with reason = %u\n", sm_event_pairing_complete_get_reason(packet));
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case SM_EVENT_REENCRYPTION_STARTED:
-                    sm_event_reencryption_complete_get_address(packet, addr);
-                    printf("ble-midi2usbhost: Bonding information exists for addr type %u, identity addr %s -> re-encryption started\n",
-                        sm_event_reencryption_started_get_addr_type(packet), bd_addr_to_str(addr));
-                    break;
-                case SM_EVENT_REENCRYPTION_COMPLETE:
-                    switch (sm_event_reencryption_complete_get_status(packet)){
-                        case ERROR_CODE_SUCCESS:
-                            printf("ble-midi2usbhost: Re-encryption complete, success\n");
-                            break;
-                        case ERROR_CODE_CONNECTION_TIMEOUT:
-                            printf("ble-midi2usbhost: Re-encryption failed, timeout\n");
-                            break;
-                        case ERROR_CODE_REMOTE_USER_TERMINATED_CONNECTION:
-                            printf("ble-midi2usbhost: Re-encryption failed, disconnected\n");
-                            break;
-                        case ERROR_CODE_PIN_OR_KEY_MISSING:
-                            printf("ble-midi2usbhost: Re-encryption failed, bonding information missing\n\n");
-                            printf("Assuming remote lost bonding information\n");
-                            printf("Deleting local bonding information to allow for new pairing...\n");
-                            sm_event_reencryption_complete_get_address(packet, addr);
-                            addr_type = sm_event_reencryption_started_get_addr_type(packet);
-                            gap_delete_bonding(addr_type, addr);
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case GATT_EVENT_QUERY_COMPLETE:
-                    status = gatt_event_query_complete_get_att_status(packet);
-                    switch (status){
-                        case ATT_ERROR_INSUFFICIENT_ENCRYPTION:
-                            printf("ble-midi2usbhost: GATT Query failed, Insufficient Encryption\n");
-                            break;
-                        case ATT_ERROR_INSUFFICIENT_AUTHENTICATION:
-                            printf("ble-midi2usbhost: GATT Query failed, Insufficient Authentication\n");
-                            break;
-                        case ATT_ERROR_BONDING_INFORMATION_MISSING:
-                            printf("ble-midi2usbhost: GATT Query failed, Bonding Information Missing\n");
-                            break;
-                        case ATT_ERROR_SUCCESS:
-                            printf("ble-midi2usbhost: GATT Query successful\n");
-                            break;
-                        default:
-                            printf("ble-midi2usbhost: GATT Query failed, status 0x%02x\n", gatt_event_query_complete_get_att_status(packet));
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            } // event_type
-            break;
-        default:
-            break;
-    } // HCI_PACKET
-}
-
-static btstack_packet_callback_registration_t sm_event_callback_registration;
 int main()
 {
     board_init();
@@ -262,29 +109,19 @@ int main()
         printf("ble-midi2usbhost: failed to initialize cyw43_arch\n");
         return -1;
     }
-    l2cap_init();
+    // No UI, just works pairing
+    ble_midi_server_init(profile_data, scan_resp_data, scan_resp_data_len,
+        IO_CAPABILITY_NO_INPUT_NO_OUTPUT,
+        SM_AUTHREQ_SECURE_CONNECTION | SM_AUTHREQ_BONDING);
 
-    sm_init();
-
-    att_server_init(profile_data, NULL, NULL);
-    // just works, legacy pairing, with bonding
-    sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
-    sm_set_authentication_requirements(SM_AUTHREQ_SECURE_CONNECTION | SM_AUTHREQ_BONDING);
-    // register for SM events
-    sm_event_callback_registration.callback = &packet_handler;
-    sm_add_event_handler(&sm_event_callback_registration);
-    midi_service_stream_init(packet_handler);
-
-    // turn on bluetooth
-    hci_power_control(HCI_POWER_ON);
     for(;;) {
         tuh_task();
         bool usb_connected = midi_dev_addr != 0 && tuh_midi_configured(midi_dev_addr);
         // poll the BLE-MIDI service and send MIDI data to USB
-        if (con_handle != HCI_CON_HANDLE_INVALID && usb_connected && tuh_midih_get_num_tx_cables(midi_dev_addr) >= 1) {
+        if (ble_midi_server_is_connected() && usb_connected && tuh_midih_get_num_tx_cables(midi_dev_addr) >= 1) {
             uint16_t timestamp;
             uint8_t mes[3];
-            uint8_t nread = midi_service_stream_read(con_handle, sizeof(mes), mes, &timestamp);
+            uint8_t nread = ble_midi_server_stream_read(sizeof(mes), mes, &timestamp);
             if (nread != 0) {
                 // Ignore timestamps for now. Handling timestamps has a few issues:
                 // 1. Some applications (e.g., TouchDAW 2.3.1 for Android or Midi Wrench on an iPad)
@@ -347,11 +184,11 @@ void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets)
                 if (bytes_read == 0)
                     return; // done
                 if (cable_num == 0) {
-                    if (con_handle == HCI_CON_HANDLE_INVALID) {
+                    if (!ble_midi_server_is_connected()) {
                         TU_LOG1("ble-midi2usbhost: No BLE-MIDI connection: Dropped %lu bytes sending to BLE-MIDI\r\n", bytes_read);
                         return;
                     }
-                    uint8_t npushed = midi_service_stream_write(con_handle, bytes_read, buffer);
+                    uint8_t npushed = ble_midi_server_stream_write(bytes_read, buffer);
                     if (npushed != bytes_read) {
                         TU_LOG1("ble-midi2usbhost: Warning: Dropped %lu bytes sending to BLE-MIDI\r\n", bytes_read - npushed);
                     }
